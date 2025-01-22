b14751878bda536e4660b17d7134776cecb8fd30
shariq-yousuf <shariqyousuf01@gmail.com>
2025-01-22 20:14:05 +0500

    chore: refactor

--- index.js
+++ index.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
+import express from "express"
+import cors from "cors"
+import dotenv from "dotenv"
+dotenv.config()
 
-var app = express();
+const app = express()
+const port = process.env.PORT || 3000
 
-app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(cors())
+app.use("/public", express.static(process.cwd() + "/public"))
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
-});
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html")
+})
 
-
-
-
-const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
-});
+  console.log("Your app is listening on port " + port)
+})
